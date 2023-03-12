21f27074ac0ea06b2be14211ce60ea6d86682549
Júlio César Mesquita Camilo Martins <juliocesarmcamilo@gmail.com>
2023-03-12 15:24:07 -0300

    Criado as rotas
    Criado as rotas do navegação para Heroes.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,14 @@ import { FormsModule } from '@angular/forms';
 import { HeroDetailsComponent } from './hero-details/hero-details.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
 import { MessagesComponent } from './messages/messages.component';
 import {MatButtonModule} from '@angular/material/button';
 import {MatExpansionModule} from '@angular/material/expansion';
+import { AppRoutingModule } from './app-routing.module';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import {MatTooltipModule} from '@angular/material/tooltip';
+
 
 @NgModule({
   declarations: [
@@ -22,9 +27,13 @@ import {MatExpansionModule} from '@angular/material/expansion';
     BrowserModule,
     FormsModule,
     BrowserAnimationsModule,
+    FlexLayoutModule,
     MatToolbarModule,
     MatButtonModule,
-    MatExpansionModule
+    MatIconModule,
+    MatExpansionModule,
+    AppRoutingModule,
+    MatTooltipModule
   ],
   providers: [],
   bootstrap: [AppComponent]
