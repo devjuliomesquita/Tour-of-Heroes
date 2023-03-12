054018b72234eba3501334479c851a6a532c394a
Júlio César Mesquita Camilo Martins <juliocesarmcamilo@gmail.com>
2023-03-12 00:57:35 -0300

    Criação do Component Details
    Criado o component Details e formamos uma relação de pai e filho.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { FormsModule } from '@angular/forms';
+import { HeroDetailsComponent } from './hero-details/hero-details.component';
 
 @NgModule({
-  declarations: [
+  declarations: [	
     AppComponent,
-    HeroesComponent
-  ],
+    HeroesComponent,
+      HeroDetailsComponent
+   ],
   imports: [
     BrowserModule,
     FormsModule
