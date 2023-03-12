f78975c665c0497cf1eede9e79b8814531c45323
Júlio César Mesquita Camilo Martins <juliocesarmcamilo@gmail.com>
2023-03-12 00:41:24 -0300

    Lista de Heroes
    Utilizado ngFor, ngIf, e o incremento de classe dinâmica com [class.nome da classe].

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Hero } from '../hero.module';
+import { Heroes } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -11,4 +12,10 @@ export class HeroesComponent {
     Id: 1,
     name: 'B de Vingança',
   } ;
+  heroes = Heroes;
+
+  selectHero?: Hero;
+  onSelect(hero: Hero): void{
+    this.selectHero = hero;
+  }
 }
---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from "./hero.module";
+
+export const Heroes: Hero[] = [
+  {Id: 1, name: 'B de Vingança'},
+  {Id: 2, name: 'Super Querer'},
+  {Id: 3, name: 'Aranhomem'},
+  {Id: 4, name: 'Homem Galinha'},
+  {Id: 5, name: 'Agiota Man'},
+  {Id: 6, name: 'Capitão Coach'},
+  {Id: 7, name: 'Lampião Verde'},
+  {Id: 8, name: 'Troço'},
+  {Id: 9, name: 'Motoqueiro do Ifood Fantasma'},
+  {Id: 10, name: 'Vó Verine'},
+];
